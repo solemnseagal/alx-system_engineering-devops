55bb98d601a164df77f824ecccdf0018885c707f
Echoda David <davidechoda123@gmail.com>
2022-01-07 00:37:43 +0100

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,64 +1,22 @@
-# Nginx server setup and configuration
-exec { 'Update the apt repository':
-  command => 'apt update',
-  path    => '/usr/bin:/usr/sbin:/bin'
+# Install and configure nginx
+package { 'jfryman-nginx':
+  ensure => installed,
 }
 
-package { 'The web server':
-  ensure          => installed,
-  name            => 'nginx',
-  provider        => 'apt',
-  install_options => ['-y']
+include nginx
+
+class { 'nginx':
+  manage_repo    => true,
+  package_source => 'nginx-stable',
 }
 
-file { 'The home page':
-  ensure  => file,
-  path    => '/var/www/html/index.html',
-  mode    => '0744',
-  owner   => 'www-data',
-  content => "Hello World!\n"
+nginx::resource::server { '34.73.76.135':
+  listen_port      => 80,
+  www_root         => '/var/www/html/',
+  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
 }
 
-file { 'The 404 page':
-  ensure  => file,
-  path    => '/var/www/error/404.html',
-  mode    => '0744',
-  owner   => 'www-data',
-  content => "Ceci n'est pas une page\n"
-}
-
-file { 'Nginx server config file':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  mode    => '0744',
-  owner   => 'www-data',
-  content =>
-"server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-
-	server_name _;
-
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-
-	if (\$request_filename ~ redirect_me){
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
-	}
-
-	error_page 404 /404.html;
-	location = /404.html {
-		root /var/www/error/;
-		internal;
-	}
-}"
-}
-
-exec { 'Start the server':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin'
+file { 'index':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Holberton School for the win!',
 }
